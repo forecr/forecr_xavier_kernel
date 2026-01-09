a76f792ba359d3f9699e0d4503d575196d923457
mdeniz0 <mehmet@forecr.io>
2026-01-09 15:34:56 +0300

    IAM20680HT included in the inv_mpu6050 driver

--- kernel/kernel-noble/drivers/iio/imu/inv_mpu6050/inv_mpu_spi.c
+++ kernel/kernel-noble/drivers/iio/imu/inv_mpu6050/inv_mpu_spi.c
@@ -80,6 +80,7 @@ static const struct spi_device_id inv_mpu_id[] = {
 	{"icm20602", INV_ICM20602},
 	{"icm20690", INV_ICM20690},
 	{"iam20680", INV_IAM20680},
+	{"iam20680ht", INV_IAM20680HT},
 	{}
 };
 
@@ -141,6 +142,10 @@ static const struct of_device_id inv_of_match[] = {
 	{
 		.compatible = "invensense,iam20680",
 		.data = (void *)INV_IAM20680
+	},
+	{
+		.compatible = "invensense,iam20680ht",
+		.data = (void *)INV_IAM20680HT
 	},
 	{ }
 };
